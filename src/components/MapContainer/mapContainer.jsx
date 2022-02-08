d4fe032e63977cf70dd8f0d0a04a7bc12c0d0064
SamWilliamsW <61912157+SamWilliamsW@users.noreply.github.com>
2022-02-08 14:10:34 -0500

    Add files via upload

---
+++ src/components/MapContainer/mapContainer.jsx
@@ -0,0 +1,29 @@
+import React, { } from 'react';
+import {  GoogleMap, LoadScript } from '@react-google-maps/api';
+
+const MapContainer = () => {
+
+    const mapStyles = {
+        height: "100vh",
+        width: "100%",
+    };
+
+    const defaultCenter = {
+        lat: 35.0459, lng: -85.2953,
+    };
+
+    return (
+        <LoadScript
+            googleMapsApiKey="AIzaSyAWxJDu4MVclbgA4OJ9TX2N5ms-w9egOMU"
+        >
+            <GoogleMap
+                mapContainerStyle={mapStyles}
+                center={defaultCenter}
+                zoom={13} />
+
+        </LoadScript>
+
+    );
+}
+
+export default MapContainer;
