1564a12422fafe98f5554ff9ba7ee301310a948b
Ally <54911412+Novally@users.noreply.github.com>
2022-03-10 11:56:54 -0800

    reducers?

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -2,5 +2,6 @@ import { combineReducers } from 'redux';
 
 import posts from './posts';
 import auth from './auth';
+import restaurants from './restaurants';
 
-export const reducers = combineReducers({ posts, auth });
+export const reducers = combineReducers({ posts, auth, restaurants });
