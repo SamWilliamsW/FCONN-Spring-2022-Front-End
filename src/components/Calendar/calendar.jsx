5ce19d315c52d8009c93c5cccb1f0aac5a4515af
pql796 <90859982+pql796@users.noreply.github.com>
2022-04-18 21:58:27 -0400

    Update calendar.jsx

--- src/components/Calendar/calendar.jsx
+++ src/components/Calendar/calendar.jsx
@@ -1,19 +1,14 @@
 import React from 'react';
-// the imports
 import { Calendar, Views, momentLocalizer  } from 'react-big-calendar' 
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import moment from 'moment'
 
+// React-big-calendar API component.  See Readme for more info
+// Basic Calendar container, call <MyCalendar/> and import to place in another component
 
 const localizer = momentLocalizer(moment)
 const MyCalendar = () => {
     
-   // const views = {
-   //     month: true,
-   //     week: true,
-   //     day: true,
-   //     agenda: true
-   // }
  
     return (
     
@@ -21,10 +16,11 @@ const MyCalendar = () => {
     localizer={localizer} 
     
     //defaultView = {'month'}
+    //events={myEventsList}
     views={[Views.MONTH, Views.WEEK, Views.DAY]}
     style={{ height: 300, width: '25%', backgroundColor: 'yellow', color: 'blue', boxSizing: 'border-box'}}
     
-    //events={myEventsList}
+ 
     startAccessor="start"
     endAccessor="end"
     
